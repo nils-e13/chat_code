79842100903c567dcfec9765ee8523d580c38482
nils-e13 <nils@sup2o.com>
2022-04-26 11:41:47 +0200

    working on a login screen + components
    both are not functional yet

--- main.js
+++ main.js
@@ -2,8 +2,30 @@
 const app = Vue.create({
     data() {
         return {
-            
+            name: "",
         }
     },
     methods: {}
 })
+
+
+/*let input = document.getElementById("login-input");
+
+function hidepage(){
+    let loginScreen = document.getElementById("login-screen");
+    loginScreen.style.display = "none";
+    mainContainer.style.display = "block";
+};
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      //document.getElementById("login-button").click();
+      loginScreen.style.display = "none";
+    }
+  });
+  */

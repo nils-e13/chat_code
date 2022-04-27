20b2313324672aa9ac88f0ffef431ef416bcf8a4
nils-e13 <nils@sup2o.com>
2022-04-27 13:29:55 +0200

    small changes but socket.io not working properly yet

--- public/components/sendMessage.js
+++ public/components/sendMessage.js
@@ -11,7 +11,7 @@ app.component('send-message', {
     /*html*/
     `<!--<div class="send-message-block">-->
     <!--v-for -> print out each content from our messages array in the chat window, in ... messages has to be defined as props above-->
-    <p class="message-content send-message-block" v-for="(content, index) in messages" :key="index"> {{ content.content }} </p>
+    <p class="message-content send-message-block no-margin" v-for="(content, index) in messages" :key="index"> {{ content.content }} </p>
     <!--</div>-->`,
     
     

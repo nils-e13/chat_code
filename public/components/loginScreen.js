1b169407bbe8f3d6944585bcbf6730906d5589eb
nils-e13 <nils@sup2o.com>
2022-05-07 02:51:02 +0200

    transformed log in screen into component

---
+++ public/components/loginScreen.js
@@ -0,0 +1,24 @@
+app.component('chat-window', {
+
+    template:
+    /*html*/
+    `
+    <!-- Login-Screen -->
+    <div class="login-screen">
+        <form class="login-form">
+            <h2 class="login-title">Welcome</h2>
+            <input class="login-input-field" type="text" id="username" placeholder="Your Name...">
+
+            <button class="login-button">Log In</button>
+        </form>
+    </div>`,
+    data() {
+        return {
+            
+
+        }
+    },
+    methods:{
+        
+        }
+    });

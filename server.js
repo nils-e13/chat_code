906595fdd31a940450bad907d7365fda1c11ed67
nils-e13 <nils@sup2o.com>
2022-05-25 14:04:36 +0200

    Commit before implementation of classes into server.js

--- server.js
+++ server.js
@@ -17,6 +17,49 @@ const dateFormat = import('dateformat');
 
 app.use(express.static(path.join(__dirname+'/public')));
 
+
+
+
+//classes for oop
+class chat {
+  constructor (uid) {
+      this._uid = uid;
+      //this.messages
+      //this.sender
+      //this.receiver
+  
+  }
+  
+}
+
+class user {
+  constructor (name) {
+      this._name = name;
+      
+  }
+
+}
+
+class message {
+  constructor (text, user) {
+      this._text = text;
+      this._user = user;
+      //this._date = date;
+  }
+
+}
+
+let testMessage = new message("hello from classes", "Nils");
+
+//let serverMessagesArray = [];
+let messageArrayClasses = [testMessage];
+//console.log(messageArrayClasses);
+
+
+
+
+
+
 //store messages + users here for now, but should be moved to classes
 let serverMessagesArray = [];
 var users = [];
